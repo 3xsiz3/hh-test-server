0833e73513580aed18a810491ef6cdd2509a95a4
3xsiz3 <93409479+3xsiz3@users.noreply.github.com>
2025-08-04 02:10:29 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -377,5 +377,18 @@ document.addEventListener('DOMContentLoaded', function() {
       }
     });
   }
-
 });
+
+
+// + block масштаба
+document.addEventListener('touchstart', function(event) {
+  if (event.touches.length > 1) {
+    event.preventDefault();
+  }
+}, { passive: false });
+
+document.addEventListener('touchmove', function(event) {
+  if (event.scale !== undefined && event.scale !== 1) {
+    event.preventDefault();
+  }
+}, { passive: false });
